c8155b8dd7fe5b614c023eb67ea101a453ddb1fb
toramasuda <tsmasuda@cpp.edu>
2017-05-30 09:11:37 -0700

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Exercise
+ {
+  public static void main(String [] args)
+   {
+    JOptionPane.showMessageDialog
+     (null,"Welcome to Java","Display Message",
+    JOptionPane.INFORMATION_MESSAGE);
+    JOptionPane.showMessageDialog
+     (null,"Welcome to Welcome to Computer Science","Display Message",
+    JOptionPane.INFORMATION_MESSAGE);
+    JOptionPane.showMessageDialog
+     (null,"Programming is Fun","Display Message",
+    JOptionPane.INFORMATION_MESSAGE);
+    
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Programming is Fun");
+    
+     System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+       System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+         System.out.println("Welcome to Java");
+  
+  
+    JOptionPane.showMessageDialog
+     (null,"Welcome to Java","Display Message",
+    JOptionPane.INFORMATION_MESSAGE);
+    
+    System.out.println("    J     A    V     v    A");
+    System.out.println("    J    A A    v   v    A A ");
+    System.out.println(" J  J   AAAAA    v v    AAAAA");
+    System.out.println("  JJ   A     A    v    A     A");
+    
+     System.exit(0);
+   }
+  }
